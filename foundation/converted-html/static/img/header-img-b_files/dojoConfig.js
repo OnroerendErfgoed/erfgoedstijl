23e76e2ab4b942c8a952ca37e5a1faeeaabc3f03
Tom Van Camp <tom.vancamp@freebility.be>
2016-03-15 15:14:41 +0100

    html export

---
+++ foundation/converted-html/static/img/header-img-b_files/dojoConfig.js
@@ -0,0 +1,18 @@
+/*jshint unused:false*/
+var dojoConfig = {
+	async: false,
+	parseOnLoad: true,
+	baseUrl: '/oeps/src',
+	tlmSiblingOfDojo: false,
+	packages: [
+		'dojo',
+		'dijit',
+		'dojox',
+		'put-selector',
+		'xstyle',
+		'dgrid',
+		'oeps',
+		'kvd',
+		'ol'
+	]
+};
